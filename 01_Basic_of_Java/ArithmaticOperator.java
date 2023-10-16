9d2981313844bc3655f9165eafab94a9744946b3
Vaibhav Wankhede <59387229+7028371795@users.noreply.github.com>
2023-10-16 15:40:29 +0000

    Add basic programm

---
+++ 01_Basic_of_Java/ArithmaticOperator.java
@@ -0,0 +1,18 @@
+
+// Arithmetic Operators
+import java.io.*;
+
+class ArithmaticOperator {
+	public static void main (String[] args) {
+		
+		int a = 10;
+		int b = 3;
+	
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+		System.out.println("a % b = " + (a % b));
+		
+	}
+}
