de3738fc726a0851cdcca3e28126bf0efe754ef3
Vaibhav Wankhede <59387229+7028371795@users.noreply.github.com>
2023-10-17 03:13:56 +0000

    Swpping of Two Number using third variable and without using third variable

---
+++ 01_Basic_of_Java/SwapTwoNoWithoutUsingThirdVar.java
@@ -0,0 +1,24 @@
+public class SwapTwoNoWithoutUsingThirdVar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Swapping without using third Variable");
+				int a=10;
+				int b=20;
+				System.out.println("Before Swappig");
+				System.out.println("a: "+a);
+				System.out.println("b: "+b);
+				a=a+b;
+				b=a-b;
+				a=a-b;
+				System.out.println("After Swappig");
+				System.out.println("a: "+a);
+				System.out.println("b: "+b);
+
+
+	
+
+
+	}
+
+}
---
+++ 01_Basic_of_Java/SwapTwoNum.java
@@ -0,0 +1,21 @@
+package basicprogram;
+
+public class SwapTwoNum {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("Before Swappig");
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+		int c=a;
+		a=b;
+		b=c;
+		System.out.println("After Swappig");
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+
+
+	}
+
+}
